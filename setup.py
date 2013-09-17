9b10689a12298e76cab350d4a4ba3c7d1e936430
aluuu <zloy.alu@gmail.com>
2013-09-17 15:49:26 +0600

    setup.py fixes

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
     author='Jordan McCoy',
     author_email='mccoy.jordan@gmail.com',
     license='BSD',
-    url='http://github.com/jordanm/django-hstore',
+    url='http://github.com/aluuu/django-hstore',
     packages=[
         'django_hstore',
+        'django_hstore.backends',
         'django_hstore.backends.postgresql_psycopg2',
+        'django_hstore.backends.gis',
         'django_hstore.backends.gis.postgresql_psycopg2',
     ],
 )
